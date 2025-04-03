67294149914bb935ec45d3f1b3f0e788321bccbf
Dr-Fortitudo <136475872+Dr-Fortitudo@users.noreply.github.com>
2025-04-03 11:16:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -36,5 +36,5 @@ def login():
     return jsonify({"message": "Invalid credentials"}), 401
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # Use Render's assigned PORT or default to 5000
+    port = int(os.environ.get("PORT", 10000))  # Use Render's assigned PORT or default to 5000
     app.run(host="0.0.0.0", port=port, debug=True)
