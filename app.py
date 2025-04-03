ceb4324f37d82d90cf37258fc2fe795a9a3e5097
Dr-Fortitudo <136475872+Dr-Fortitudo@users.noreply.github.com>
2025-04-03 13:14:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
+import os
+import json
 from flask import Flask, jsonify
 import firebase_admin
 from firebase_admin import credentials, firestore
 
-# Initialize Flask App
 app = Flask(__name__)
 
-# Load Firebase Credentials
-cred = credentials.Certificate("timetable-b3371-firebase-adminsdk-fbsvc-d13120ed4b.json") 
+# Load Firebase Credentials from Environment Variable
+firebase_credentials = json.loads(os.getenv("FIREBASE_CREDENTIALS"))
+cred = credentials.Certificate(firebase_credentials)
 firebase_admin.initialize_app(cred)
+
 db = firestore.client()
 
 @app.route("/")
