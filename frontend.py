6c6bf71a8b1af3c1cf97845b308b8f42e5e4c65e
Dr-Fortitudo <136475872+Dr-Fortitudo@users.noreply.github.com>
2025-04-03 13:38:30 +0530

    Update frontend.py

--- frontend.py
+++ frontend.py
@@ -1,10 +1,11 @@
 import streamlit as st
 import requests
 
-st.title("Automated Timetable System")
+# Streamlit UI
+st.title("Faculty Login")
 
 # Backend URL
-API_URL = "http://127.0.0.1:5000/"
+API_URL = "https://automatic-timetable-generator-2953.onrender.com"
 
 # Fetch data from backend
 response = requests.get(API_URL)
@@ -13,3 +14,26 @@ if response.status_code == 200:
     st.write(f"Backend Response: {data['message']}")
 else:
     st.write("Error connecting to the backend.")
+
+Login_URL = "https://automatic-timetable-generator-2953.onrender.com" 
+
+def login(username, password):
+    response = requests.post(Login_URL, json={"username": username, "password": password})
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return None
+
+username = st.text_input("Username")
+password = st.text_input("Password", type="password")
+login_button = st.button("Login")
+
+if login_button:
+    result = login(username, password)
+    if result:
+        st.success(f"Welcome {result['role']}!") 
+        st.session_state["logged_in"] = True
+        st.session_state["user"] = result
+        st.experimental_rerun()
+    else:
+        st.error("Invalid username or password")
